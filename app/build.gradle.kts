2948e3b2a713ef9180c3eaa739b66c0db0cf6108
PaladinDev <paladingames1@gmail.com>
2024-02-01 18:04:17 +0300

    Release 1.0.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "me.paladin.wifi"
         minSdk = 21
         targetSdk = 34
-        versionCode = 8
-        versionName = "1.0.7"
+        versionCode = 9
+        versionName = "1.0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
